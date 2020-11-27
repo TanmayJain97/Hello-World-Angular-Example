ef0cd52cc254b33e2cfd46b0e8df1fd2b44bc4b5
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-11-28 00:49:11 +0530

    Add: Regex check to validate User Name
    Updated as per UC 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="../assets/Logo/BridgeLabz New Logo.svg";
   url="https://www.google.co.in/";
   userName: string = "Tanmay";
+  nameError: string = "";
 
   ngOnInit():void {
     this.title="Bridgelabz says Hello";
@@ -18,5 +19,19 @@ export class AppComponent {
   onClick($event){
     console.log("A button is clicked!",$event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event){
+    console.log("Change Event Occured!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$'); 
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    if(this.userName==""){
+      this.nameError = "Empty Field!!";
+      return;
+    }
+    this.nameError = "Name is Invalid!";
   }
 }
